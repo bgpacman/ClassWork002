5b41a67e0eca77ca55dede88c9f4849659b43dc0
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-11-22 20:15:20 +0200

    java

--- src/main.java
+++ src/main.java
@@ -1,4 +1,20 @@
 
 public class main {
+	
+	public static void main(String[] args) {
+
+		int num = 51;
+		System.out.println(num);
+
+		int newNum = num;
+		System.out.println(newNum);
+
+		num = 102;
+		System.out.println(num);
+		System.out.println(newNum);
+
+		// int d;
+		// System.out.println(d);
+	}
 
 }
